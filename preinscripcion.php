bba6ea1be1a8dbe28bf5ab5e78feace2e7bdbbfa
Hector Idme <felipeidme04@gmail.com>
2020-09-10 12:04:38 -0500

    version1.0

---
+++ preinscripcion.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+Preinscripción<br>
+Nombre: <?php echo $_POST["nombre"]; ?><br>
+Apellidos: <?php echo $_POST["apellido"]; ?><br>
+DNI: <?php echo $_POST["DNI"]; ?><br>
+E-mail: <?php echo $_POST["email"]; ?><br>
+Paquete: <?php echo $_POST["paquete"]; ?><br>
+
+</body>
+</html>
